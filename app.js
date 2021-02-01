0d1fba9f6985d7ebf7872abfe32b8a5ec3e78546
LPT <lptalty@gmail.com>
2021-02-01 13:21:49 -0500

    computer selection function

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+function computerSelection(){
+    //randomly return rock, paper, or scissors
+    let computerChoice = Math.floor(Math.random() * Math.floor(3));
+    if(computerChoice === 0){
+        computerChoice = 'rock'
+    }else if(computerChoice === 1){
+        computerChoice = 'paper'
+    }else{
+        computerChoice = 'scissors'
+    }
+    return computerChoice;
+}
+
+function playRound(computerSelection, playerSelection){
+}
+
+console.log(computerSelection());
